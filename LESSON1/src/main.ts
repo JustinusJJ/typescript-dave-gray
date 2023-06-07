528ad9f181b2d7c73963736de87878cfefeb01fb
JustinusJJ <john.justinus@gmail.com>
2023-06-07 14:43:20 +0700

    L1: noEmitOnError
    Won't generate JS if there's an error in TS

--- LESSON1/src/main.ts
+++ LESSON1/src/main.ts
@@ -1,6 +1,9 @@
 let username = 'John'
 console.log(username)
 
-let a = 12
-let b = '6'
-let c = 2
+let a: number = 12
+let b: number = 6
+let c: number = 2
+
+console.log(a / b)
+console.log(c * b)
